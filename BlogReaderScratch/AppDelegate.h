7a977f609984981883e8e4f4ca3ad9e93d16e70a
Jesus Morales <jesusam@umich.edu>
2013-05-07 14:25:41 -0400

    Blog reaer with user interface formed from scratch

---
+++ BlogReaderScratch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlogReaderScratch
+//
+//  Created by Jesus Morales on 5/7/13.
+//  Copyright (c) 2013 Jesus Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlogReaderScratch/TableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TableViewController.h
+//  BlogReaderScratch
+//
+//  Created by Jesus Morales on 5/7/13.
+//  Copyright (c) 2013 Jesus Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *blogPosts;
+
+@end
